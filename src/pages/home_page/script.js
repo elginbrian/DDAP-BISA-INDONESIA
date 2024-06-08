08ecaf7bf856f11e918e264c30384a7390049dd1
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-06-08 19:43:54 +0700

    add news

--- src/pages/home_page/script.js
+++ src/pages/home_page/script.js
@@ -1,5 +1,6 @@
 function toggleSections() {
   var sections = document.querySelectorAll(".visi-section, .misi-section, .goals-section");
+  var newsSection = document.querySelector(".news-section");
 
   sections.forEach(function (section) {
     section.classList.toggle("hidden");
@@ -8,5 +9,16 @@ function toggleSections() {
 
   var button = document.getElementById("toggle-button");
   var isHidden = sections[0].classList.contains("hidden");
+
+  if (!isHidden) {
+    newsSection.classList.toggle("hidden-news");
+  } else if (isHidden) {
+    newsSection.classList.toggle("hidden-news");
+  }
   button.textContent = isHidden ? "Show More" : "Show Less";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var newsSection = document.querySelector(".news-section");
+  newsSection.classList.toggle("hidden-news");
+});

397bc1b52eb5894dd58e270cb282c5dda02bf92d
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-06-08 20:15:32 +0700

    add BottomHero

--- src/pages/home_page/script.js
+++ src/pages/home_page/script.js
@@ -1,6 +1,7 @@
 function toggleSections() {
   var sections = document.querySelectorAll(".visi-section, .misi-section, .goals-section");
   var newsSection = document.querySelector(".news-section");
+  var bottomHero = document.querySelector(".bottom-hero");
 
   sections.forEach(function (section) {
     section.classList.toggle("hidden");
@@ -12,13 +13,17 @@ function toggleSections() {
 
   if (!isHidden) {
     newsSection.classList.toggle("hidden-news");
+    bottomHero.classList.toggle("hidden-news");
   } else if (isHidden) {
     newsSection.classList.toggle("hidden-news");
+    bottomHero.classList.toggle("hidden-news");
   }
   button.textContent = isHidden ? "Show More" : "Show Less";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   var newsSection = document.querySelector(".news-section");
+  var bottomHero = document.querySelector(".bottom-hero");
   newsSection.classList.toggle("hidden-news");
+  bottomHero.classList.toggle("hidden-news");
 });
